01284d26d420da4cd071fd52459787100b805a58
Rodrigo Gouveia <rgestevao@gmail.com>
2024-06-16 15:38:24 -0300

    feat: implementing get products flow

--- src/main/java/com/api/data_cache/api/ProductController.java
+++ src/main/java/com/api/data_cache/api/ProductController.java
@@ -6,9 +6,12 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
@@ -24,5 +27,18 @@ public class ProductController {
         var productResponse = productService.createProduct(productTO);
         log.info("Finishing product created - {}", productResponse.toString());
         return ResponseEntity.status(HttpStatus.CREATED).body(productResponse);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductTO>> getProducts(String name, Integer offset, Integer limit) {
+        log.info("Getting products - Name: {}, Offset: {}, Limit: {}", name, offset, limit);
+        var products = productService.getProducts(name, offset, limit);
+        log.info("Finishing getting products - Size: {}, Name: {}, Offset: {}, Limit: {}",
+                products.size(),
+                name,
+                offset,
+                limit
+        );
+        return ResponseEntity.status(HttpStatus.OK).body(products);
     }
 }
--- src/main/java/com/api/data_cache/service/ProductService.java
+++ src/main/java/com/api/data_cache/service/ProductService.java
@@ -1,11 +1,21 @@
 package com.api.data_cache.service;
 
+import com.api.data_cache.db.Product;
 import com.api.data_cache.db.ProductRepository;
 import com.api.data_cache.dto.ProductTO;
 import com.api.data_cache.mapper.ProductMapper;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import lombok.extern.log4j.Log4j2;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Log4j2
@@ -17,6 +27,9 @@ public class ProductService {
     @Autowired
     private ProductMapper productMapper;
 
+    @Autowired
+    private EntityManager entityManager;
+
     public ProductTO createProduct(ProductTO productTO) {
         log.info("Initializing create product flow - {}", productTO.toString());
         var product = productMapper.fromProductTO(productTO);
@@ -26,5 +39,36 @@ public class ProductService {
         var productResponse = productMapper.fromProduct(productCreated);
         log.info("Finishing create product flow - {}", productResponse.toString());
         return productResponse;
+    }
+
+    public List<ProductTO> getProducts(String name, Integer offset, Integer limit) {
+        log.info("Initializing get products flow - Name: {}, Offset: {}, Limit: {}", name, offset, limit);
+        var criteriaBuilder = entityManager.getCriteriaBuilder();
+        var criteriaQuery = criteriaBuilder.createQuery(Product.class);
+        var root = criteriaQuery.from(Product.class);
+        var predicates = buildPredicates(criteriaBuilder, root, name);
+        criteriaQuery.select(root).where(predicates.toArray(new Predicate[0]));
+        var productQuery = entityManager.createQuery(criteriaQuery);
+        productQuery.setFirstResult(offset);
+        productQuery.setMaxResults(limit);
+        var products = productQuery.getResultList();
+        var response = buildProducts(products);
+        log.info("Finishing get products flow - Size: {}", response.size());
+        return response;
+    }
+
+    private List<Predicate> buildPredicates(CriteriaBuilder criteriaBuilder,
+                                            Root<Product> root,
+                                            String name) {
+        log.info("Building predicates to get products Name: {}", name);
+        var predicates = new ArrayList<Predicate>();
+        if (StringUtils.isNotBlank(name)) {
+            predicates.add(criteriaBuilder.like(criteriaBuilder.lower(root.get("name")), "%" + name.toLowerCase() + "%"));
+        }
+        return predicates;
+    }
+
+    private List<ProductTO> buildProducts(List<Product> products) {
+        return products.stream().map(productMapper::fromProduct).toList();
     }
 }
